a956d7df8479d37a5761e9632b37063356847547
Matheus Macedo <matheus.macedo.df@hotmail.com>
2025-09-03 17:22:08 -0300

    Foi incluindo mais testes da funcao de soma, solicitado pelo Julio como desafio

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,4 +1,3 @@
-const { describe } = require('mocha')
 const { somarDoisNumeros } = require('../src/calculadora')
 const { expect, assert } = require('chai')
 
@@ -10,11 +9,25 @@ describe ('Testes da Função de soma', () => {
     expect(resultadoDaSoma).to.equal(8)
   })
 
-  it ('A funcao deve ser capaz de somar um numero positivo e um negativo' , () => {
+  it ('A Função deve ser capaz de somar um numero positivo e um negativo' , () => {
       // Coletar o resultado da Função
       const resultadoDaSoma = somarDoisNumeros (50, -15)
       // Compara o reultado com o valor que você espera
       expect(resultadoDaSoma).to.equal(35)
+  })
+
+    it ('A Função deve ser capaz de somar dois numeros zerados' , () => {
+      // Coletar o resultado da Função
+      const resultadoDaSoma = somarDoisNumeros (0, 0)
+      // Compara o reultado com o valor que você espera
+      expect(resultadoDaSoma).to.equal(0)
+  })
+
+    it ('A Função deve ser capaz de somar dois numeros negativos' , () => {
+      // Coletar o resultado da Função
+      const resultadoDaSoma = somarDoisNumeros (-40, -15)
+      // Compara o reultado com o valor que você espera
+      expect(resultadoDaSoma).to.equal(-55)
   })
 
 })
